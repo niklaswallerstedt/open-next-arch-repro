e7e4adc46a164666d1bca8718b5fa578290b6132
Niklas Wallerstedt <niklaswallerstedt@gmail.com>
2024-11-03 13:20:35 +0100

    Use same open-next version

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,8 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
+const OPEN_NEXT_AWS_VERSION = "3.2.0" as const;
+
 export default {
   config() {
     return {
@@ -10,7 +12,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const buildCommand =
+        `sst bind --stage ${app.stage} "pnpx @opennextjs/aws@${OPEN_NEXT_AWS_VERSION} build"` as const;
+
+      const site = new NextjsSite(stack, "site", {
+        buildCommand,
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
